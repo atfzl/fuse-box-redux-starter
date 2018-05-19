ed58539846e128689f0c229c8c59ed4f54bbf83f
Atif Afzal <atif5801@gmail.com>
2018-05-19 23:16:40 +0530

    Add react-router-dom

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,13 +1,21 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import { setStatefulModules } from '~/ui/hmr';
 import store from '~/ui/store';
 
 ReactDOM.render(
-  <Provider store={store as any}>
-    <div>Hello</div>
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store as any}>
+      <div>
+        <div>Hello</div>
+        <Link to="/bar">foo</Link>
+        <Route path="/" exact render={() => <div>home</div>} />
+        <Route path="/bar" exact render={() => <div>bar</div>} />
+      </div>
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('app'),
 );
 
